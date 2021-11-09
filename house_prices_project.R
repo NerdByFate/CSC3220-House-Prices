0183bec6209f87748332cfe73bf64265403c2d20
Cherokee Parker <5161492+NerdByFate@users.noreply.github.com>
2021-11-09 13:08:15 -0600

    Load Packages and Data

---
+++ house_prices_project.R
@@ -0,0 +1,12 @@
+# Filename: house_prices_project.R
+# Author: CSC 3220 House Prices Team
+# Date: 11/8/2021
+# Purpose: 
+
+# Load prerequisite packages
+library(readr)
+library(ggplot2)
+
+# Load in data
+test = read_csv("test.csv")
+train = read_csv("train.csv")
