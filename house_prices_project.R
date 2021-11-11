cfbe1da4bf55e0835886f942a63b644fd67a8322
Mdee7 <83259006+Mdee7@users.noreply.github.com>
2021-11-11 13:04:30 -0600

    EDA 2
    EDA for numerical variables

--- house_prices_project.R
+++ house_prices_project.R
@@ -8,12 +8,25 @@ library(readr)
 library(ggplot2)
 
 # Load in data
-test = read_csv("test.csv")
-train = read_csv("train.csv")
+test <- read_csv("C:\\Users\\amina\\Downloads\\New folder (5)\\test.csv")
+train = read_csv("C:\\Users\\amina\\Downloads\\New folder (5)\\train.csv")
 
-y = train$SalePrice
+#-------------------
+summary(test)
+hist(train$Id) #id got a wierd distribution
+hist((train$MSSubClass)) # the shape is mostly falling down to the rightware
+hist(train$LotFrontage)
+hist(train$LotArea)
+hist(train$OverallCond)
+hist(train$OverallQual)
+hist(train$YearBuilt)
+hist(train$YearRemodAdd)
+hist(train$MasVnrArea)
+hist(train$BsmtFinSF1)
+hist(train$BsmtFinSF2)
+hist(train$BsmtUnfSF)
+hist(train$TotalBsmtSF)
+hist(train$LowQualFinSF)
+hist(train$GrLivArea)
 
-#--------------------------------------------------
-#                       EDA
-#--------------------------------------------------
-summary(train)
+
